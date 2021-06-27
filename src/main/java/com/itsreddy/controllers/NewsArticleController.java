ef0b76437f925b8d2e8152009003467f1db6af69
itsreddy <dprashureddy007@gmail.com>
2021-06-26 23:16:56 -0700

    added cross origin support for the controller

--- src/main/java/com/itsreddy/controllers/NewsArticleController.java
+++ src/main/java/com/itsreddy/controllers/NewsArticleController.java
@@ -3,6 +3,7 @@ package com.itsreddy.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import com.itsreddy.model.ArticleWrapper;
 
 
 @RestController
+@CrossOrigin
 public class NewsArticleController {
 	
 	@Autowired
